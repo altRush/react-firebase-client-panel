1b39838a36d9a0b29b877ef560de13a1d6a770e2
Rush WP <rushing.wp@gmail.com>
2018-12-11 14:34:48 +0700

    Initiate

---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { createStore, combineReducers, compose } from 'redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
